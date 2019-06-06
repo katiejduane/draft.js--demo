9f802e180e76623b03bbef6e7d33c3ebfd650887
Katie Duane <katiejduane@gmail.com>
2019-06-05 20:30:18 -0400

    teeny tiny text editor in browser

---
+++ src/components/PageContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Editor, EditorState } from 'draft-js';
+
+
+class PageContainer extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            editorState: EditorState.createEmpty()
+        }
+    }
+
+    onChange = (editorState) => {
+        this.setState({
+            editorState
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <Editor 
+                    editorState={this.state.editorState}
+                    onChange={this.onChange}
+                    />
+            </div>
+        )
+    }
+};
+
+export default PageContainer;
