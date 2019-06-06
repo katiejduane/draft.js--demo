e34f192cb3d4768e9e8e8826ac9764ba96e94357
Katie Duane <katiejduane@gmail.com>
2019-06-06 15:15:58 -0400

    highligting plugin added

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import { Editor, EditorState, RichUtils } from 'draft-js';
+import { EditorState, RichUtils } from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import createHighlightPlugin from './plugins/highlightPlugin'
 
 import styles from './PageContainer.module.css';
 
+const highlightPlugin = createHighlightPlugin();
 
 class PageContainer extends Component{
     constructor(props){
         super(props)
         this.state = {
             editorState: EditorState.createEmpty()
-        }
+        };
+        this.plugins = [
+            highlightPlugin,
+        ];
     }
 
     onChange = (editorState) => {
@@ -40,16 +46,23 @@ class PageContainer extends Component{
         this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
     }
 
+    onHighlight = () => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'HIGHLIGHT'))
+    }
+
     render(){
         return(
             <div className={styles.EditorContainer}>
                 <button onClick={this.onItalicClick}><em>I</em></button>
                 <button onClick={this.onUnderlineClick}>U</button>
                 <button onClick={this.onBoldClick}><strong>B</strong></button>
+                <button className="highlight" onClick={this.onHighlight}>
+                    <span style={{ background: "yellow" }}>H</span>
+                </button>
                 <Editor 
                     editorState={this.state.editorState}
-                    handleKeyCommand={this.handleKeyCommand}
                     onChange={this.onChange}
+                    plugins={this.plugins}
                 />
             </div>
         )
