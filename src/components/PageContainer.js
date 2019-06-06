21c8073a3411f26c89d4132dde38a4dcfa8693e2
Katie Duane <katiejduane@gmail.com>
2019-06-05 20:48:10 -0400

    buttons for bold, italic and underline added

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 
 class PageContainer extends Component{
@@ -13,16 +13,42 @@ class PageContainer extends Component{
     onChange = (editorState) => {
         this.setState({
             editorState
-        });
+        })
+    }
+
+    handleKeyCommand = (command) => {
+        const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
+        if(newState){
+            this.onChange(newState);
+            return 'handled';
+        }else{
+            return 'not handled';
+        }
+    }
+
+    onItalicClick = () => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'))
+    }
+
+    onUnderlineClick = () => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'))
+    }
+
+    onBoldClick = () => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
     }
 
     render(){
         return(
             <div>
+                <button onClick={this.onItalicClick}><em>I</em></button>
+                <button onClick={this.onUnderlineClick}>U</button>
+                <button onClick={this.onBoldClick}><strong>B</strong></button>
                 <Editor 
                     editorState={this.state.editorState}
+                    handleKeyCommand={this.handleKeyCommand}
                     onChange={this.onChange}
-                    />
+                />
             </div>
         )
     }
